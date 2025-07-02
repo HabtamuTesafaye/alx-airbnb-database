8c072be2ebd9ab7165d54144541d09b6dc944bc2
Habtamu Tesfaye <habtamutesfaye678@gmail.com>
2025-07-02 09:29:22 +0300

    feat: updated the left join query

--- database-adv-script/joins_queries.sql
+++ database-adv-script/joins_queries.sql
@@ -3,10 +3,16 @@ SELECT b.*, u.first_name, u.last_name, u.email
 FROM bookings b
 INNER JOIN users u ON b.user_id = u.user_id;
 
--- LEFT JOIN: All properties and their reviews
-SELECT p.*, r.rating, r.comment
+-- LEFT JOIN: All properties and their reviews, including properties with no reviews
+SELECT 
+  p.property_id,
+  p.name AS property_name,
+  r.rating,
+  r.comment
 FROM properties p
-LEFT JOIN reviews r ON p.property_id = r.property_id;
+LEFT JOIN reviews r ON p.property_id = r.property_id
+ORDER BY p.property_id;
+
 
 -- FULL OUTER JOIN: All users and all bookings
 SELECT u.user_id, u.first_name, b.booking_id, b.property_id, b.start_date, b.end_date
